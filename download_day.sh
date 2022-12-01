8165d63da997347256bc4aa029ac0009fa98b9f7
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2022-12-01 23:43:39 +0100

    day1 solution in julia fixes

--- download_day.sh
+++ download_day.sh
@@ -2,13 +2,14 @@
 
 read -p "December day: " DAY_NUM
 
-DAY_DIR="$HOME/advent_of_code/day$DAY_NUM"
+YEAR=2022
+DAY_DIR="$HOME/git/advent_of_code/$YEAR/day$DAY_NUM"
 if [[ !( -d $DAY_DIR ) ]]; then
   # If directory does not exist - create it
   echo "Creating directory '$DAY_DIR'..."
   mkdir $DAY_DIR
 fi
 
-SESSION_COOKIE=`cat $HOME/advent_of_code/session_cookie.txt`
+SESSION_COOKIE=`cat $HOME/git/advent_of_code/session_cookie.txt`
 echo "Downloading data into '$DAY_DIR/data.txt'..."
-curl -H "Cookie: $SESSION_COOKIE" https://adventofcode.com/2022/day/$DAY_NUM/input > "$DAY_DIR/data.txt"
+curl -H "Cookie: $SESSION_COOKIE" https://adventofcode.com/$YEAR/day/$DAY_NUM/input > "$DAY_DIR/data.txt"
