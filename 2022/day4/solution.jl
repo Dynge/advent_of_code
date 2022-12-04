65718aec7443a4d89add6d4cc1f5a276f9a728a5
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2022-12-04 22:48:31 +0100

    added type hints and renamed a few variables

--- 2022/day4/solution.jl
+++ 2022/day4/solution.jl
@@ -1,25 +1,24 @@
 # Import data
-data = open("/home/michael/git/advent_of_code/2022/day4/data.txt", "r");
-raw_cleanup_pairs = eachline(data)
+data = open(readlines, "/home/michael/git/advent_of_code/2022/day4/data.txt", "r");
 
 ####################
 println("Part 1:")
 
 # Clean data -- one matrix representing the first elves and another for all the seconds elves in the cleanup pairs.
-cleanup_pairs_vector = map(x -> split(x, ","), raw_cleanup_pairs)
-cleanup_pairs_matrix = reduce(hcat,cleanup_pairs_vector)
-cleanup_elf_one_range = reduce(hcat, map(x -> parse.(Int, x), map(x -> split(x, "-"), cleanup_pairs_matrix[1,:])))
-cleanup_elf_two_range = reduce(hcat, map(x -> parse.(Int, x), map(x -> split(x, "-"), cleanup_pairs_matrix[2,:])))
+cleanup_pairs_vector::Vector{Vector{AbstractString}} = map(x -> split(x, ","), data)
+cleanup_pairs_matrix::Matrix{AbstractString} = reduce(hcat,cleanup_pairs_vector)
+cleanup_elf_one_range::Matrix{Int8} = reduce(hcat, map(x -> parse.(Int, x), map(x -> split(x, "-"), cleanup_pairs_matrix[1,:])))
+cleanup_elf_two_range::Matrix{Int8} = reduce(hcat, map(x -> parse.(Int, x), map(x -> split(x, "-"), cleanup_pairs_matrix[2,:])))
 
 # Subtract the beginning number of each elf from their partners beginning number
 # Subtract the end number of each elf from their partners end number
 # Finally convert to sign value (-1, 0 or +1).
 difference_matrix = map(sign, cleanup_elf_one_range - cleanup_elf_two_range)
 
-function contained_count(matrix::Array)::Int
+function contained_count(matrix::Matrix)::Int
   # Sum matrix cols
-  # If sum is less than two they are fully contained within
-  return sum(map(x -> abs(x) < 2, sum(matrix, dims=1)))
+  # If absolute sum is less than two they are fully contained within
+  return sum(map(col_sum -> abs(col_sum) < 2, sum(matrix, dims=1)))
 end
 
 println(
