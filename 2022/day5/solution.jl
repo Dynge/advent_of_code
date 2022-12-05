a6d44cf9b5950008a05c12a2b8106fd0cf96a9a1
Michael Damsbo Lyngs <mdl@dataproces.dk>
2022-12-05 13:21:53 +0100

    added solution for day5

---
+++ 2022/day5/solution.jl
@@ -0,0 +1,83 @@
+function extract_blocks(row_block_in_strings)
+  crate_vectors::Vector{Vector{Char}} = [[] for _ in 1:9]
+  data_cols = collect(2:4:length(row_block_in_strings[1])-1)
+  for string in reverse(row_block_in_strings)
+    for (block_num, col) in enumerate(data_cols)
+      if string[col] != ' '
+        append!(crate_vectors[block_num], string[col])
+      end
+    end
+  end
+
+  return crate_vectors
+end
+
+
+function extract_moves(move_data_strings)
+  moves_vectors::Vector{Vector{Int64}} = map(x -> parse.(Int, x),
+      map(split,
+          map(x -> replace(x, r"[^0-9 ]" => ""), move_data_strings)
+         )
+    )
+  return moves_vectors
+    
+end
+
+
+function perform_moves_cratemover_9000(crate_vectors, moves_vector)
+  copy_vectors = deepcopy(crate_vectors)
+  for (count, from, to) in moves_vector
+    for _ in 1:count
+      char = pop!(copy_vectors[from])
+      append!(copy_vectors[to], char)
+    end
+  end
+  return copy_vectors
+end
+
+
+function perform_moves_cratemover_9001(crate_vectors, moves_vector)
+  copy_vectors = deepcopy(crate_vectors)
+  for (count, from, to) in moves_vector
+    move_list = copy_vectors[from][end-(count-1):end]
+    copy_vectors[from] = copy_vectors[from][begin:end-count]
+    append!(copy_vectors[to], move_list)
+  end
+  return copy_vectors
+end
+
+
+function main()
+  # Import data
+  data = open(readlines, "./2022/day5/data.txt", "r");
+  row_block_strings = map(x -> replace(x, r"\[|\]" => " "), data[1:8])
+
+  #################
+  println("Part 1:")
+
+  crate_vectors = extract_blocks(row_block_strings)
+  moves_vector = extract_moves(data[11:end])
+
+  cratemover9000_vectors = perform_moves_cratemover_9000(crate_vectors, moves_vector)
+  top_crates = join(map(pop!, cratemover9000_vectors))
+
+  println(
+          "The top crates with CrateMover9000 are as follows: ",
+          top_crates
+         )
+
+  ###################
+  println("Part 2:")
+
+  cratemover9001_vectors = perform_moves_cratemover_9001(crate_vectors, moves_vector)
+  top_crates = join(map(pop!, cratemover9001_vectors))
+
+  println(
+          "The top crates with CrateMover9001 are as follows: ",
+          top_crates
+         )
+
+end
+
+
+@time main()
