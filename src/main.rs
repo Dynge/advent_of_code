b21cc3edce6a72c6af76150e201545c522aecffc
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2022-12-06 20:51:35 +0100

    added rust for day6 and day1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,66 @@
+use std::fs;
+use std::collections::HashSet;
+use std::iter::Iterator;
+
 fn main() {
-    let a = String::from("hello");
-    let b = a;
-    println!("Hello, world!");
-    println!("{b}");
-    other()
+    let rs_day1 = day1();
+    let rs_day6 = day6();
+    println!("Day 1 Results - {:?}", rs_day1);
+    println!("Day 6 Results - {:?}", rs_day6);
 }
 
-fn other() {
-    let something: [i32; 4] = [1,2,3,4];
-    let d = something.map(|x| x.to_string()).join("");
-    println!("{d}");
+fn day1() -> Vec<i32> {
+    let data = fs::read_to_string("./2022/day1/data.txt").expect("Couldn't read data :(");
+
+    let mut result: Vec<i32> = vec![];
+
+    // Part 1
+    let elves = data.trim_end().split("\n\n");
+    let combined_elf_food_list: Vec<Vec<i32>> = elves
+        .map(|elf| elf.split("\n")
+            .map(|item| item.parse().unwrap())
+            .collect())
+        .collect();
+
+    let mut elf_sums: Vec<i32> = vec![];
+    for elf_store in combined_elf_food_list {
+        elf_sums.push(elf_store.iter().sum())
+    }
+
+    result.push(*elf_sums.iter().max().unwrap());
+
+    // Part 2
+    elf_sums.sort();
+    let top3_sums = &elf_sums[elf_sums.len()-3..].iter().sum();
+
+    result.push(*top3_sums);
+
+    return result;
 }
+
+fn day6() -> Vec<i32> {
+    let data = fs::read_to_string("./2022/day6/data.txt").expect("Couldn't read data :(");
+
+    let mut result: Vec<i32> = vec![];
+
+    // Part 1
+    for index in 4..data.len() {
+        let chars: HashSet<_> = data[index-4..index].chars().collect();
+        if chars.len() == 4 {
+            result.push(index.try_into().unwrap());
+            break
+        }
+    }
+
+    // Part 2
+    for index in 14..data.len() {
+        let chars: HashSet<_> = data[index-14..index].chars().collect();
+        if chars.len() == 14 {
+            result.push(index.try_into().unwrap());
+            break
+        }
+    }
+
+    return result
+}
+
