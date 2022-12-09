bcef855b36fd1442c5626f897ffdafdd24e06c40
Michael Damsbo Lyngs <mdl@dataproces.dk>
2022-12-09 11:05:54 +0100

    fixed advent today

--- 2022/day8/solution.jl
+++ 2022/day8/solution.jl
@@ -1,3 +1,5 @@
+using IterTools
+
 function main()
   # Import data
   data = open(readlines, "./2022/day8/data.txt", "r");
@@ -7,69 +9,89 @@ function main()
   vec_of_vec = map(row -> [parse(Int8, c) for c in row], data)
   forest = reduce(vcat,transpose.(vec_of_vec))
 
-  min_cols = [min(row...) for row in eachrow(hcat(forest[1, 2:end-1], forest[end, 2:end-1]))]
-  min_rows = [min(row...) for row in eachrow(hcat(forest[2:end-1, 1], forest[2:end-1, end]))]
-
   interior_forest = forest[2:end-1,2:end-1]
 
 
-  global interior_seeable_count = 0
-
+  interior_seeable_count = 0
   for row in axes(interior_forest, 1)
     for col in axes(interior_forest, 2)
       current_tree = interior_forest[row,col]
-      seeable = true
-
-      for row_back in 1:row
-        if forest[row_back,col+1] >= current_tree
-          
-          seeable = false
-        end
-      end
-      if seeable
-        global interior_seeable_count += 1
-        continue
-      end
-
-      for row_forward in row+2:size(forest, 1)
-        if forest[row_forward,col+1] >= current_tree
-          
-          seeable = false
-        end
-      end
-      if seeable
-        global interior_seeable_count += 1
-        continue
-      end
-
-      for col_back in 1:col
-        if forest[row+1,col_back] >= current_tree
-          
-          seeable = false
-        end
-      end
-      if seeable
-        global interior_seeable_count += 1
-        continue
-      end
-
-      for col_forward in col+2:size(forest, 2)
-        if forest[row+1,col_forward] >= current_tree
-          
-          seeable = false
-        end
-      end
-      if seeable
-        global interior_seeable_count += 1
-        continue
-      end
+      interior_seeable_count +=  can_see(current_tree, (row+1, col+1), forest)
     end
   end
 
+  println(interior_seeable_count + 99 + 99 + 97 + 97)
 
-  return interior_seeable_count + 99 + 99 + 97 + 97
+  ############
+  println("Part 2:")
+
+  results::Vector{Int64} = [0]
+  for row in axes(interior_forest, 1)
+    for col in axes(interior_forest, 2)
+      current_tree = interior_forest[row,col]
+      push!(results, scenic_score(current_tree, (row+1, col+1), forest))
+    end
+  end
+
+  println(maximum(results))
+
+end
+
+function can_see(tree, position, forest)
+  up = forest[position[1], 1:position[2]-1] .< tree
+  down = forest[position[1], position[2]+1:end] .< tree
+  left = forest[1:position[1]-1, position[2]] .< tree
+  right = forest[position[1]+1:end, position[2]] .< tree
+
+  return any([all(down), all(up), all(left), all(right)])
+end
 
 
+function scenic_score(tree, position, forest)
+  up = 
+    collect(
+      takewhile(neighbor_tree -> neighbor_tree < tree, forest[position[1], position[2]-1:-1:1])
+    )
+  down = 
+    collect(
+      takewhile(neighbor_tree -> neighbor_tree < tree,forest[position[1], position[2]+1:end])
+    )
+  left = 
+    collect(
+      takewhile(neighbor_tree -> neighbor_tree < tree, forest[position[1]-1:-1:1, position[2]])
+    )
+  right = 
+    collect(
+      takewhile(neighbor_tree -> neighbor_tree < tree,forest[position[1]+1:end, position[2]])
+    )
+
+  score = 1
+
+  if length(up) ==  position[2] - 1
+    score *= length(up)
+  else
+    score *= length(up) + 1
+  end
+
+  if length(down) == size(forest, 2) - position[2]
+    score *= length(down)
+  else
+    score *= length(down) + 1
+  end
+  if length(left) == position[1] - 1
+    score *= length(left)
+  else
+    score *= length(left) + 1
+  end
+  if length(right) == size(forest, 1) - position[1]
+    score *= length(right)
+  else
+    score *= length(right) + 1
+  end
+
+  println(score)
+
+  return score
 end
 
 @time main()
