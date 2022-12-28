f53b212977465a2a15d85259f163f45ac8790e82
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2022-12-28 14:21:57 +0100

    day17 almost done

--- src/y2022/day17.rs
+++ src/y2022/day17.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, collections::HashSet};
 
 use ndarray::arr2;
 
@@ -13,85 +13,161 @@ pub fn day17() -> [i32; 2]{
     let structures: Vec<Vec<i32>> = vec![
         vec![16+8+4+2],// 0011110
         vec![
+            8,         // 0001000
+            16+8+4,    // 0011100
+            8,         // 0001000
+        ], vec![
+            4,          // 0000100
+            4,          // 0000100
+            16+8+4,    // 0011100
+        ],
+        vec![
             16,         // 0010000
             16,         // 0010000
             16,         // 0010000
             16,         // 0010000
         ], 
         vec![
-            8,         // 0001000
-            16+8+4,    // 0011100
-            8,         // 0001000
-        ],
-        vec![
             16+8,      // 0011000
             16+8,      // 0011000
-        ],
-        vec![
-            4,          // 0000100
-            4,          // 0000100
-            16+8+4,    // 0011100
         ],
     ];
 
     let mut tower: Vec<i32> = vec![64+32+16+8+4+2+1]; // 1111111
 
-    let mut movement_iter = data.chars();
+    let mut movement_iter = data.chars().enumerate().cycle().peekable();
     let mut current_structure_index = 0;
-    for _ in 0..2022 {
-
+    let loop_count: u64 = 1_000_000_000_000;
+    let mut cache = HashSet::<(usize, Vec<i32>, usize)>::new();
+    let mut structure_count = 0;
+    while structure_count < loop_count {
         let mut structure = structures[current_structure_index].clone();
-        for _ in 0..3 {
-            structure = move_structure(&structure, &movement_iter.next().unwrap());
-        }
 
-        let mut not_at_rest: bool = true;
+        let mut falling_down: bool = true;
 
-        tower.append(&mut structure);
-
-        let mut tower_structure_indices: Vec<Option<usize>> = vec![None; structure.len()];
-        while not_at_rest {
-            for index in tower_structure_indices.iter() {
-                match index {
-                    Some(x) => {
-                        if (tower[*x] | structure.last().unwrap()) == 0i32 {
-
-                        }
-                    },
-                    None => continue,
-                };
-
+        if tower.len() > 20 {
+            let (_, tower_cache) = tower.split_at(tower.len()-20);
+            let (direction_index, _) = movement_iter.peek().unwrap().clone();
+            // println!("tower cache {:?}", tower_cache);
+            let new_cache = cache.insert((current_structure_index, tower_cache.to_vec(), direction_index));
+            if new_cache == false  {
+                println!("Wow a loop");
+                // Loop
+                let remaining_loops = loop_count / structure_count;
+                // TODO: Calculate height instead of making a stupidly large vector
+                // tower = tower.repeat(remaining_loops as usize);
+                structure_count = loop_count - (loop_count - structure_count * remaining_loops);
+                cache = HashSet::new();
+                println!("{} a loop", structure_count);
             }
-
-
-
         }
+        println!("count {}", structure_count);
+
+        let mut structure_lowest_index: usize = tower.len().clone() + 3;
+        while falling_down {
+            let (_, next_direction) = &movement_iter.next().unwrap();
+            let mut should_move_structure = true;
+
+            // Wind
+            for (index, line) in structure.iter().rev().enumerate() {
+                let tower_structure_index = index + structure_lowest_index;
+                if tower_structure_index >= tower.len() {
+                    continue;
+                }
+                if cannot_move_structure(line, &tower[tower_structure_index], &next_direction) {
+                    should_move_structure = false;
+                }
+            }
+            if should_move_structure {
+                move_structure(&mut structure, next_direction)
+            }
+            // for line in structure.iter() {
+                // println!("Structure: {:b}", line);
+            // }
+
+
+            // Fall down
+            should_move_structure = true;
+            for (index, line) in structure.iter().rev().enumerate() {
+                let tower_structure_index = index + (structure_lowest_index-1);
+                if tower_structure_index >= tower.len() {
+                    continue;
+                }
+
+                if cannot_move_structure(line, &tower[tower_structure_index], &'v') {
+                    should_move_structure = false;
+                }
+            }
+            if should_move_structure {
+                structure_lowest_index -= 1;
+            } else {
+                // Insert structure onto tower
+                for (index, line) in structure.iter().rev().enumerate() {
+                    let tower_structure_index = index + (structure_lowest_index);
+                    if tower_structure_index >= tower.len() {
+                        // println!("{}", line.clone());
+                        tower.push(line.clone());
+                        continue;
+                    }
+
+                    tower[tower_structure_index] = tower[tower_structure_index] | line
+                }
+                falling_down = false;
+            }
+        }
+        structure_count += 1;
 
         current_structure_index = (current_structure_index + 1) % structures.len();
-
-
     }
 
+    // for line in tower.iter().rev() {
+    //     println!("Tower: {:b}", line);
+    // }
 
-    println!("Move Left {}", (2 << 1)); // = 4
-    println!("Move Right {}", (4 >> 1)); // = 2
 
-
-    return [0, 0]
+    return [tower.len() as i32 - 1, 0]
 }
 
-fn move_structure(structure: &Vec<i32>, direction: &char) -> Vec<i32> {
-    let mut moved_structure = vec![];
-    if direction == &'>' {
-        for line in structure {
-            moved_structure.push(line >> 1);
-        }
-    } else if direction == &'<' {
-        for line in structure {
-            moved_structure.push(line << 1)
-        }
+fn move_structure(structure: &mut Vec<i32>, direction: &char) {
+    match direction {
+        &'>' => {
+            if structure.clone().iter().all(|line| line.clone() % 2 == 0) {
+                // Does not touch right side
+                for line in structure.iter_mut() {
+                    *line = *line >> 1;
+                }
+            }
+        },
+        &'<' => {
+            if structure.clone().iter().all(|line| line.clone() < 64) {
+                // Does not touch left side
+                for line in structure.iter_mut() {
+                    *line = *line << 1;
+                }
+            }
+        },
+        _ => panic!("Cannot move by this direction {}", direction),
     }
+}
 
-    return moved_structure;
-
+fn cannot_move_structure(structure_line: &i32, tower_line: &i32, direction: &char) -> bool {
+    match direction {
+        &'>' => {
+            if structure_line >> 1 & tower_line == 0i32 {
+                return false;
+            } 
+        },
+        &'<' => {
+            if structure_line << 1 & tower_line == 0i32 {
+                return false;
+            } 
+        },
+        &'v' => {
+            if structure_line & tower_line == 0i32 {
+                return false;
+            } 
+        }
+        _ => panic!("Dont know how to check a move by this direction {}", direction),
+    }
+    return true;
 }
