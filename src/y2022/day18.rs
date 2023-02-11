93d9576427b5949e6ef2a41779d29bc0d82f945e
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2023-02-11 15:08:38 +0100

    formatting

--- src/y2022/day18.rs
+++ src/y2022/day18.rs
@@ -1,23 +1,27 @@
-use std::{fs, collections::{HashSet, VecDeque}};
-
+use std::{
+    collections::{HashSet, VecDeque},
+    fs,
+};
 
 pub fn day18() -> [u32; 2] {
     let data = fs::read_to_string("./2022/day18.txt").expect("Cannot read file");
 
-    let bricks = data.trim_end().split("\n")
+    let bricks = data
+        .trim_end()
+        .split("\n")
         .map(|line| {
-            let mut line_iter = line.split(",")
-                .map(|coor| 
-                    coor.parse::<i16>().unwrap()
-                ).into_iter();
+            let mut line_iter = line
+                .split(",")
+                .map(|coor| coor.parse::<i16>().unwrap())
+                .into_iter();
 
             [
                 line_iter.next().unwrap(),
                 line_iter.next().unwrap(),
-                line_iter.next().unwrap()
+                line_iter.next().unwrap(),
             ]
-        }).collect::<HashSet<_>>();
-
+        })
+        .collect::<HashSet<_>>();
 
     let start_position: [i16; 3] = [0; 3];
     let mut visited = HashSet::<[i16; 3]>::from([start_position.clone()]);
@@ -25,7 +29,6 @@ pub fn day18() -> [u32; 2] {
 
     let mut all_exposed_walls = 0u32;
     let mut exposed_walls_exterior = 0u32;
-
 
     while queue.len() > 0 {
         // BFS
@@ -36,17 +39,14 @@ pub fn day18() -> [u32; 2] {
         // println!("neighbor len {:?}", neighbors);
         all_exposed_walls += hit_bricks;
 
-        neighbors
-            .iter()
-            .for_each(|neighbor|  {
-                queue.push_back(neighbor.clone());
-                visited.insert(neighbor.clone());
-            });
+        neighbors.iter().for_each(|neighbor| {
+            queue.push_back(neighbor.clone());
+            visited.insert(neighbor.clone());
+        });
     }
 
     let mut visited = HashSet::<[i16; 3]>::from([start_position.clone()]);
     let mut queue = VecDeque::<[i16; 3]>::from([start_position.clone()]);
-
 
     while queue.len() > 0 {
         // BFS
@@ -57,14 +57,11 @@ pub fn day18() -> [u32; 2] {
         // println!("neighbor len {:?}", neighbors);
         exposed_walls_exterior += hit_bricks;
 
-        neighbors
-            .iter()
-            .for_each(|neighbor|  {
-                queue.push_back(neighbor.clone());
-                visited.insert(neighbor.clone());
-            });
+        neighbors.iter().for_each(|neighbor| {
+            queue.push_back(neighbor.clone());
+            visited.insert(neighbor.clone());
+        });
     }
-
 
     return [all_exposed_walls, exposed_walls_exterior];
 }
@@ -78,21 +75,22 @@ fn find_all_neighbors(
     let mut hit_bricks = 0u32;
     let neighbor_positions: Vec<[i16; 3]> = vec![
         [-1, 0, 0],
-        [ 1, 0, 0],
+        [1, 0, 0],
         [0, -1, 0],
-        [0,  1, 0],
+        [0, 1, 0],
         [0, 0, -1],
-        [0, 0,  1],
-    ].iter()
-        .map(|neighbor_rel| {
-            [
-                neighbor_rel[0] + coordinate[0],
-                neighbor_rel[1] + coordinate[1],
-                neighbor_rel[2] + coordinate[2]
-            ]
-        })
-        .filter(|neighbor_abs| neighbor_abs.iter().any(|coor| coor < &-5 || coor > &25) == false)
-        .collect();
+        [0, 0, 1],
+    ]
+    .iter()
+    .map(|neighbor_rel| {
+        [
+            neighbor_rel[0] + coordinate[0],
+            neighbor_rel[1] + coordinate[1],
+            neighbor_rel[2] + coordinate[2],
+        ]
+    })
+    .filter(|neighbor_abs| neighbor_abs.iter().any(|coor| coor < &-5 || coor > &25) == false)
+    .collect();
 
     for neighbor in neighbor_positions.iter() {
         if bricks.contains(neighbor) && !bricks.contains(coordinate) {
@@ -104,11 +102,8 @@ fn find_all_neighbors(
         valid_neighbors.push(neighbor.clone());
     }
 
-    
     return (hit_bricks, valid_neighbors);
-
 }
-
 
 fn find_neighbors_exterior(
     coordinate: &[i16; 3],
@@ -119,21 +114,22 @@ fn find_neighbors_exterior(
     let mut hit_bricks = 0u32;
     let neighbor_positions: Vec<[i16; 3]> = vec![
         [-1, 0, 0],
-        [ 1, 0, 0],
+        [1, 0, 0],
         [0, -1, 0],
-        [0,  1, 0],
+        [0, 1, 0],
         [0, 0, -1],
-        [0, 0,  1],
-    ].iter()
-        .map(|neighbor_rel| {
-            [
-                neighbor_rel[0] + coordinate[0],
-                neighbor_rel[1] + coordinate[1],
-                neighbor_rel[2] + coordinate[2]
-            ]
-        })
-        .filter(|neighbor_abs| neighbor_abs.iter().any(|coor| coor < &-5 || coor > &25) == false)
-        .collect();
+        [0, 0, 1],
+    ]
+    .iter()
+    .map(|neighbor_rel| {
+        [
+            neighbor_rel[0] + coordinate[0],
+            neighbor_rel[1] + coordinate[1],
+            neighbor_rel[2] + coordinate[2],
+        ]
+    })
+    .filter(|neighbor_abs| neighbor_abs.iter().any(|coor| coor < &-5 || coor > &25) == false)
+    .collect();
 
     for neighbor in neighbor_positions.iter() {
         if bricks.contains(neighbor) {
@@ -146,7 +142,5 @@ fn find_neighbors_exterior(
         valid_neighbors.push(neighbor.clone());
     }
 
-    
     return (hit_bricks, valid_neighbors);
-
 }
