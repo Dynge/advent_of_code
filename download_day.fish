9cd2d464cb4236f1d7a8e00b7c847ea7aaf69421
Michael Damsbo Lyngs <michael_lyngs@outlook.dk>
2023-02-11 15:07:50 +0100

    rewrote download in fish syntax

--- download_day.fish
+++ download_day.fish
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/fish
 
-read -p "December day: " DAY_NUM
+read -l -P "December day: " DAY_NUM
 
-YEAR=2022
-YEAR_DIR="$HOME/git/advent_of_code/$YEAR"
-if [[ !( -d $YEAR_DIR ) ]]; then
+set YEAR 2022
+set YEAR_DIR "$HOME/git/advent_of_code/$YEAR"
+if not test -d $YEAR_DIR
   # If directory does not exist - create it
   echo "Creating directory '$YEAR_DIR'..."
   mkdir $YEAR_DIR
-fi
+end
 
-SESSION_COOKIE=`cat $HOME/git/advent_of_code/session_cookie.txt`
+set SESSION_COOKIE (cat $HOME/git/advent_of_code/session_cookie.txt)
 echo "Downloading data into '$YEAR_DIR/day$DAY_NUM.txt'..."
 curl -H "Cookie: $SESSION_COOKIE" https://adventofcode.com/$YEAR/day/$DAY_NUM/input > "$YEAR_DIR/day$DAY_NUM.txt"
